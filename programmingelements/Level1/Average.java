4c378a172e3bc000cb31ad0592e0f2b710ba2948
aarushi07-tech <aarushi.sharma_cs.aiml24@gla.ac.in>
2025-08-21 11:16:46 +0530

    [aarushi] added file

---
+++ programmingelements/Level1/Average.java
@@ -0,0 +1,15 @@
+package programmingelements.Level1;
+
+public class Average {
+    
+    public static void main(String[] args) {
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+        int numberOfSubjects = 3;
+        double average = (maths + physics + chemistry) / (double) numberOfSubjects;
+        System.out.println("Sam’s average mark in PCM is " + average);
+    }    
+}
+
+
