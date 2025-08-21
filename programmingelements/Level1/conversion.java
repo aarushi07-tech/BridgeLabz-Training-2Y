ed04cae41335e74a405c11a3f7810d9e9c350005
aarushi07-tech <aarushi.sharma_cs.aiml24@gla.ac.in>
2025-08-21 11:21:01 +0530

    [aarushi] added file

---
+++ programmingelements/Level1/conversion.java
@@ -0,0 +1,19 @@
+package programmingelements.Level1;
+
+public class conversion {
+    public static void main(String[] args) {
+        // Distance in kilometers
+        double kilometers = 10.8;
+
+        // Conversion factor
+        double conversionFactor = 1.6;
+
+        // Convert to miles
+        double miles = kilometers * conversionFactor;
+
+        // Display the result
+        System.out.println("The distance " + kilometers + " km in miles is " + miles);
+    }
+
+    
+}
