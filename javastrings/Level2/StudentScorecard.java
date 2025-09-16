e4feb070ea24c51f9fc6cba32cc058f81d28a5c5
aarushi07-tech <aarushi.sharma_cs.aiml24@gla.ac.in>
2025-09-16 12:10:03 +0530

    [aarushi] added file

---
+++ javastrings/Level2/StudentScorecard.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class StudentScorecard {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of students: ");
+        int n = scanner.nextInt();
+
+        int[][] scores = generateScores(n);
+        double[][] stats = calculateStats(scores);
+        String[] grades = calculateGrades(stats);
+        displayScorecard(scores, stats, grades);
+        scanner.close();
+    }
+
+    public static int[][] generateScores(int n) {
+        Random random = new Random();
+        int[][] scores = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            scores[i][0] = random.nextInt(90) + 10;
+            scores[i][1] = random.nextInt(90) + 10;
+            scores[i][2] = random.nextInt(90) + 10;
+        }
+        return scores;
+    }
+
+    public static double[][] calculateStats(int[][] scores) {
+        double[][] stats = new double[scores.length][3];
+        for (int i = 0; i < scores.length; i++) {
+            int total = scores[i][0] + scores[i][1] + scores[i][2];
+            double average = total / 3.0;
+            double percentage = (total / 300.0) * 100;
+            stats[i][0] = total;
+            stats[i][1] = Math.round(average * 100.0) / 100.0;
+            stats[i][2] = Math.round(percentage * 100.0) / 100.0;
+        }
+        return stats;
+    }
+
+    public static String[] calculateGrades(double[][] stats) {
+        String[] grades = new String[stats.length];
+        for (int i = 0; i < stats.length; i++) {
+            double percentage = stats[i][2];
+            if (percentage >= 90) {
+                grades[i] = "A+";
+            } else if (percentage >= 80) {
+                grades[i] = "A";
+            } else if (percentage >= 70) {
+                grades[i] = "B";
+            } else if (percentage >= 60) {
+                grades[i] = "C";
+            } else if (percentage >= 50) {
+                grades[i] = "D";
+            } else {
+                grades[i] = "F";
+            }
+        }
+        return grades;
+    }
+
+    public static void displayScorecard(int[][] scores, double[][] stats, String[] grades) {
+        System.out.println("Student\tPhysics\tChemistry\tMaths\tTotal\tAverage\tPercentage\tGrade");
+        for (int i = 0; i < scores.length; i++) {
+            System.out.println(
+                (i + 1) + "\t" +
+                scores[i][0] + "\t" +
+                scores[i][1] + "\t\t" +
+                scores[i][2] + "\t" +
+                (int)stats[i][0] + "\t" +
+                stats[i][1] + "\t" +
+                stats[i][2] + "%\t\t" +
+                grades[i]
+            );
+        }
+    }
+}
