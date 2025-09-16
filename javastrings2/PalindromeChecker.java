948552a97098aa6fe0326b0f672b38fcf40a707a
aarushi07-tech <aarushi.sharma_cs.aiml24@gla.ac.in>
2025-09-16 12:55:21 +0530

    [aarushi] added file

---
+++ javastrings2/PalindromeChecker.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        boolean isPalindrome = checkPalindrome(input);
+        if (isPalindrome) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+        scanner.close();
+    }
+
+    public static boolean checkPalindrome(String str) {
+        str = str.toLowerCase().replaceAll("\\s+", "");
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
