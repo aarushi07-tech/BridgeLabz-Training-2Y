7330a4e24cd5f8f919cac88588b048db876d7356
aarushi07-tech <aarushi.sharma_cs.aiml24@gla.ac.in>
2025-09-16 12:32:10 +0530

    [aarushi] added file

---
+++ javastrings/Level3/FreqCount.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class FreqCount {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        String[] frequencyResult = getCharacterFrequency(input);
+        displayFrequency(frequencyResult);
+        scanner.close();
+    }
+
+    public static String[] getCharacterFrequency(String str) {
+        char[] chars = str.toCharArray();
+        int[] freq = new int[chars.length];
+
+        for (int i = 0; i < chars.length; i++) {
+            freq[i] = 1;
+            if (chars[i] == '0') {
+                continue;
+            }
+            for (int j = i + 1; j < chars.length; j++) {
+                if (chars[i] == chars[j]) {
+                    freq[i]++;
+                    chars[j] = '0';
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] != '0') {
+                count++;
+            }
+        }
+
+        String[] result = new String[count];
+        int index = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] != '0') {
+                result[index] = chars[i] + " = " + freq[i];
+                index++;
+            }
+        }
+
+        return result;
+    }
+
+    public static void displayFrequency(String[] result) {
+        System.out.println("Character Frequencies:");
+        for (String entry : result) {
+            System.out.println(entry);
+        }
+    }
+}
