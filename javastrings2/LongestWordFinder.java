c5bb3c2a8a28edbce33948196de71b7d5395c0e6
aarushi07-tech <aarushi.sharma_cs.aiml24@gla.ac.in>
2025-09-18 10:38:32 +0530

    [aarushi] added file

---
+++ javastrings2/LongestWordFinder.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class LongestWordFinder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String sentence = scanner.nextLine();
+        String[] words = sentence.split("\\s+");
+        String longestWord = "";
+        for (String word : words) {
+            word = word.replaceAll("[^a-zA-Z]", "");
+            if (word.length() > longestWord.length()) {
+                longestWord = word;
+            }
+        }
+        System.out.println("The longest word is: " + longestWord);
+        scanner.close();
+    }
+}
